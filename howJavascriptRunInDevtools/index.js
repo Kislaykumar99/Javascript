0f40b65ed24860d12dbe55010df7bb4f99c2090b
Kislay Kumar <kumarkislay402@gmail.com>
2024-06-09 09:09:00 +0530

    first commit

---
+++ howJavascriptRunInDevtools/index.js
@@ -0,0 +1,5 @@
+var firstName='Kislay'
+let lastName='Kumar'
+let age=24;
+const yearOfBith=2000
+let intro= 'I am '+ firstName +' '+lastName 
---
+++ script.js
@@ -0,0 +1,6 @@
+// console.log("Hello World!")
+
+let firstName='Kislay'
+let lastName='Kumar'
+let age=24;
+let intro= 'I am '+ firstName +' '+lastName+' ' +'I am '+ age +' years old.'

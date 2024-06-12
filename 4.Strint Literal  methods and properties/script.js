246a118bae13160fc966c89834665581e7716a89
Kislay Kumar <kumarkislay402@gmail.com>
2024-06-13 02:55:54 +0530

    Strinhg properties and methods

---
+++ 4.Strint Literal  methods and properties/script.js
@@ -0,0 +1,6 @@
+const message ="Hello"
+const CapitalMessage=message.toUpperCase()
+const messageWithSpace="     My name is Kislay Kumar       "
+const finalMessage=messageWithSpace.trim().toLowerCase()
+
+isStringExist=finalMessage.includes("my name is")

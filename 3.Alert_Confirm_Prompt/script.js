6c2c64f9586bcba491f57ba6f79e3b78d753d8bc
Kislay Kumar <kumarkislay402@gmail.com>
2024-07-13 00:15:09 +0530

    Javascript math

--- 3.Alert_Confirm_Prompt/script.js
+++ 3.Alert_Confirm_Prompt/script.js
@@ -1,4 +1,4 @@
-// const alertResult=alert("new message")
+const alertResult=alert("new message")
 const isConfirm=confirm("do you want to proceed?")
-// const userInput =prompt("Enter your name ")
-// console.log(userInput)
+const userInput =prompt("Enter your name ")
+console.log(userInput)
---
+++ 5. Math Object In Javascript/script.js
@@ -0,0 +1,5 @@
+const height= +prompt("Enter Height of rectangle") //using + to convert string to number
+const width= +prompt("Enter width of rectangle")
+
+
+document.write(`Area : ${height*width}`)
